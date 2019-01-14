4e59348aed3e833db3ea459b1db68570eb0f010f
jist <jistjoalal@gmail.com>
2019-01-14 00:14:57 -0800

    adds simpl-schema to project and tests it out

--- server/main.js
+++ server/main.js
@@ -1,5 +1,56 @@
 import { Meteor } from 'meteor/meteor';
+import SimpleSchema from 'simpl-schema';
 
 Meteor.startup(() => {
   // code to run on server at startup
+
+  const petSchema = new SimpleSchema({
+    name: {
+      type: String,
+      min: 1,
+      max: 20,
+      optional: true,
+    },
+    age: {
+      type: Number,
+      min: 0,
+    },
+    contactNumber: {
+      type: String,
+      optional: true,
+      regEx: SimpleSchema.RegEx.Phone,
+    }
+  });
+
+  petSchema.validate({
+    age: 2,
+  });
+
+  // employeeSchema
+  // name - 1-200, required
+  // hourlyWage - >0
+  // email - regex
+  const employeeSchema = new SimpleSchema({
+    name: {
+      type: String,
+      min: 1,
+      max: 200,
+    },
+    hourlyWage: {
+      type: Number,
+      min: 1,
+      optional: true,
+    },
+    email: {
+      type: String,
+      optional: true,
+      regEx: SimpleSchema.RegEx.Email,
+    },
+  });
+
+  employeeSchema.validate({
+    name: 'swag man',
+    hourlyWage: 1,
+    email: 'asdf@b.com',
+  });
 });

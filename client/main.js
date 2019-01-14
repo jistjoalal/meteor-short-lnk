15090ac71eab427411e0261b9312e97c13a840cc
jist <jistjoalal@gmail.com>
2019-01-13 23:31:40 -0800

    switches to replace based redirects - fixes broken back button from history stack filling up

--- client/main.js
+++ client/main.js
@@ -2,7 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { Tracker } from 'meteor/tracker';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 
 import Signup from '../imports/ui/Signup';
@@ -10,14 +10,28 @@ import Link from '../imports/ui/Link';
 import NotFound from '../imports/ui/NotFound';
 import Login from '../imports/ui/Login';
 
+// basic auth
+const onEnterPublicPage = Component => {
+  if (Meteor.userId()) {
+    return <Redirect to="/links" />;
+  }
+  return <Component />
+}
+const onEnterPrivatePage = Component => {
+  if (!Meteor.userId()) {
+    return <Redirect to="/" />;
+  }
+  return <Component />
+}
+
 // routes
 const history = createBrowserHistory();
 const AppRouter = () => (
   <Router history={history}>
     <Switch>
-      <Route path="/" exact component={Login} />
-      <Route path="/signup" component={Signup} />
-      <Route path="/links" component={Link} />
+      <Route path="/" exact render={() => onEnterPublicPage(Login)} />
+      <Route path="/signup" render={() => onEnterPublicPage(Signup)} />
+      <Route path="/links" render={() => onEnterPrivatePage(Link)} />
       <Route component={NotFound} />
     </Switch>
   </Router>
@@ -26,17 +40,16 @@ const AppRouter = () => (
 // redirects
 const unauthenticatedPages = ['/', '/signup'];
 const authenticatedPages = ['/links']
-
 Tracker.autorun(() => {
   const isAuthenticated = !!Meteor.userId();
   const pathName = history.location.pathname;
   const isUnauthenticatedPage = unauthenticatedPages.includes(pathName);
   const isAuthenticatedPage = authenticatedPages.includes(pathName);
   if (isUnauthenticatedPage && isAuthenticated) {
-    history.push('/links');
+    history.replace('/links');
   }
   if (isAuthenticatedPage && !isAuthenticated) {
-    history.push('/');
+    history.replace('/');
   }
 });
 

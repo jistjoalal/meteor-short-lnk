1f8eb24ed639795ecc01bdc8508822e9af7eb6c5
jist <jistjoalal@gmail.com>
2019-01-18 13:22:56 -0800

    basic login/signup styles

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -30,19 +30,21 @@ class Login extends Component {
   }
   render() {
     return (
-      <React.Fragment>
-        <h1>Short Lnk</h1>
+      <div className="boxed-view">
+        <div className="boxed-view__box">
+          <h1>Short Lnk</h1>
 
-        { !this.state.error ? null : <p>{this.state.error}</p> }
+          { !this.state.error ? null : <p>{this.state.error}</p> }
 
-        <form onSubmit={this.onSubmit} noValidate>
-          <input type="email" ref="email" name="email" placeholder="Email" />
-          <input type="password" ref="password" name="password" placeholder="Password" />
-          <button>Login</button>
-        </form>
+          <form onSubmit={this.onSubmit} noValidate>
+            <input type="email" ref="email" name="email" placeholder="Email" />
+            <input type="password" ref="password" name="password" placeholder="Password" />
+            <button>Login</button>
+          </form>
 
-        <Link to="/signup">Register</Link>
-      </React.Fragment>
+          <Link to="/signup">Register</Link>
+        </div>
+      </div>
     );
   }
 }
--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -38,19 +38,21 @@ class Signup extends Component {
   }
   render() {
     return (
-      <React.Fragment>
-        <h1>Signup</h1>
+      <div className="boxed-view">
+        <div className="boxed-view__box">
+          <h1>Signup</h1>
 
-        { !this.state.error ? null : <p>{this.state.error}</p> }
+          { !this.state.error ? null : <p>{this.state.error}</p> }
 
-        <form onSubmit={this.onSubmit} noValidate>
-          <input type="email" ref="email" name="email" placeholder="Email" />
-          <input type="password" ref="password" name="password" placeholder="Password" />
-          <button>Create Account</button>
-        </form>
+          <form onSubmit={this.onSubmit} noValidate>
+            <input type="email" ref="email" name="email" placeholder="Email" />
+            <input type="password" ref="password" name="password" placeholder="Password" />
+            <button>Create Account</button>
+          </form>
 
-        <Link to="/">Login</Link>
-      </React.Fragment>
+          <Link to="/">Login</Link>
+        </div>
+      </div>
     );
   }
 }

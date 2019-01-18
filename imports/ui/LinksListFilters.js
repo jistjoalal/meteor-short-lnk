cc011b3b37a4603f4d6345ca82baff69338ecf56
jist <jistjoalal@gmail.com>
2019-01-18 06:55:02 -0800

    controlled inputs using reactive meteor session

--- imports/ui/LinksListFilters.js
+++ imports/ui/LinksListFilters.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { Component }  from 'react';
 import { Session } from 'meteor/session';
+import { Tracker } from 'meteor/tracker';
 
-export default props =>
-  <div>
-    <label>
-      <input type="checkbox" onChange={e => {
-        Session.set('showVisible', !e.target.checked);
-      }} />
-      show hidden links
-    </label>
-  </div>
+export default class LinksListFilter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showVisible: true,
+    };
+  }
+  componentDidMount() {
+    this.filterTracker = Tracker.autorun(() => {
+      this.setState({
+        showVisible: Session.get('showVisible'),
+      });
+    });
+  }
+  componentWillUnmount() {
+    this.filterTracker.stop();
+  }
+  render() {
+    return (
+      <div>
+        <label>
+          <input type="checkbox" checked={!this.state.showVisible} onChange={e => {
+            Session.set('showVisible', !e.target.checked);
+          }} />
+          show hidden links
+        </label>
+      </div>
+    );
+  }
+}

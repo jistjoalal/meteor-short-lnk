0a0fdde20a2718e3881620aed2b12a05bd44796b
jist <jistjoalal@gmail.com>
2019-01-18 13:50:15 -0800

    more login/signup styles

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -36,7 +36,7 @@ class Login extends Component {
 
           { !this.state.error ? null : <p>{this.state.error}</p> }
 
-          <form onSubmit={this.onSubmit} noValidate>
+          <form className="boxed-view__form" onSubmit={this.onSubmit} noValidate>
             <input type="email" ref="email" name="email" placeholder="Email" />
             <input type="password" ref="password" name="password" placeholder="Password" />
             <button>Login</button>
--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -44,7 +44,7 @@ class Signup extends Component {
 
           { !this.state.error ? null : <p>{this.state.error}</p> }
 
-          <form onSubmit={this.onSubmit} noValidate>
+          <form className="boxed-view__form" onSubmit={this.onSubmit} noValidate>
             <input type="email" ref="email" name="email" placeholder="Email" />
             <input type="password" ref="password" name="password" placeholder="Password" />
             <button>Create Account</button>
